e57caf01d2c423f5254a2c5dfbd9490c91df728f
Ronell Bradley <ronellbradley@gmail.com>
2026-03-20 15:04:39 -0400

    feat: import enhancements.css in main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,18 @@ import '../styles/responsive.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+import React from 'react';
+    import ReactDOM from 'react-dom/client';
+    import App from './App.jsx';
+    import '../styles/main.css';
+    import '../styles/responsive.css';
+    import '../styles/enhancements.css';
+    
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <React.StrictMode>
+          <App />
+      </React.StrictMode>React.StrictMode>
+  );</React.StrictMode><App />
   </React.StrictMode>
 );
 

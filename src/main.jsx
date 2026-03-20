3878d826c575ea5bdf3c505f2dbb9823ce1cae69
Ronell Bradley <ronellbradley@gmail.com>
2026-03-20 15:14:57 -0400

    fix: clean up corrupted main.jsx, add enhancements.css import

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import '../styles/main.css';
 import '../styles/responsive.css';
+import '../styles/enhancements.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-import React from 'react';
-    import ReactDOM from 'react-dom/client';
-    import App from './App.jsx';
-    import '../styles/main.css';
-    import '../styles/responsive.css';
-    import '../styles/enhancements.css';
-    
-    ReactDOM.createRoot(document.getElementById('root')).render(
-      <React.StrictMode>
-          <App />
-      </React.StrictMode>React.StrictMode>
-  );</React.StrictMode><App />
+    <App />
   </React.StrictMode>
 );
-
-
-
-
